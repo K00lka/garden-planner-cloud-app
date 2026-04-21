51f31cb9a2b0b53dc344b659c5503a5a2017d4b5
K00lka <69790586+K00lka@users.noreply.github.com>
2026-04-21 14:18:38 +0200

    readme

--- TaskManager.Tests/UnitTest1.cs
+++ TaskManager.Tests/UnitTest1.cs
@@ -1,10 +1,15 @@
-﻿namespace TaskManager.Tests;
+﻿using Xunit;
+using CloudBackend.Models;
+
+namespace TaskManager.Tests;
 
 public class UnitTest1
 {
+   
     [Fact]
     public void Test1()
     {
-
+        var task = new CloudTask { Name = "Przetestować bezpiecznik" };
+        Assert.False(task.IsCompleted);
     }
 }
